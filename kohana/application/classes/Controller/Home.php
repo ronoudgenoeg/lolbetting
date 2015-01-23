325f37ca510d819303fd874ecf0dda5e1ce3a4c5
gebruiker <gebruiker@192.168.2.3>
2015-01-23 16:24:05 +0100

    User ORM template

--- kohana/application/classes/Controller/Home.php
+++ kohana/application/classes/Controller/Home.php
@@ -4,13 +4,17 @@ class Controller_Home extends Controller {
 
 	public function action_index()
 	{
-		$user = new Model_User();
-		$user->username = 'Dmagoz';
-		$user->email = 'ronoudgenoeg@gmail.com';
-		$user->password = '123';
-		$user->save();
+// 		$user = new Model_User();
+// 		$user->username = 'Dmagoz';
+// 		$user->email = 'ronoudgenoeg@gmail.com';
+// 		$user->password = '123';
+// 		$user->save();
 		
-		
+// 		$user = ORM::factory('user', 'ronoudgenoeg@gmail.com');
+// 		if($user->loaded()){
+// 			$user->username = 'dmagoz_changed';
+// 			$user->save();
+// 		}
 		
 		$view = View::factory('core/template');
 		$view->title = "Welcome!";
